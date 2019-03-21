480403d072b1732f6c0badc2ad5f6c6085d428fe
michal1domanski <michal.domanski22@gmail.com>
2019-03-21 13:28:05 +0100

    get_float change

--- lab2/lab1.py
+++ lab2/lab1.py
@@ -1,19 +1,18 @@
 from numpy import *
+from cs50 import get_float
 
-print("X circle radius")
-radiusx = float(input())
-while radiusx < 0:
-    print("X circle radius")
-    radiusx = float(input())
+radiusx = get_float('X circle radius: ')
+while radiusx <= 0:
+    print("Wrong try again")
+    radiusx = get_float('X circle radius: ')
 
-print("Y circle radius")
-radiusy = float(input())
-while radiusy < 0:
-    print("Y circle radius")
-    radiusy = float(input())
+radiusy = get_float('Y circle radius: ')
+while radiusy <= 0:
+    print("Wrong try again")
+    radiusy = get_float('Y circle radius: ')
 
-print(f"Field of circle X is {pi*(radiusx)**2:.2f}, field of circle X is {2*pi*radiusx:.2f}")
-print(f"Field of circle Y is {pi*(radiusy)**2:.2f}, field of circle Y is {2*pi*radiusy:.2f}")
+print(f"Field of circle X is {pi*(radiusx)**2:.2f}, perimeter of circle X is {2*pi*radiusx:.2f}")
+print(f"Field of circle Y is {pi*(radiusy)**2:.2f}, permieter of circle Y is {2*pi*radiusy:.2f}")
 
 print(f"Field of X and Y ring is {abs(pi*(radiusx)**2 - pi*(radiusy)**2):.2f}")
 print(f"Perimeter of X and Y ring is {2*pi*radiusx + 2*pi*radiusy:.2f}")

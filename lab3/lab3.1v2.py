8811984fe4a51f70863b589d594d96eca4a97c6f
michal1domanski <michal.domanski22@gmail.com>
2019-03-21 14:00:54 +0100

    3.1 v2

---
+++ lab3/lab3.1v2.py
@@ -0,0 +1,30 @@
+from cs50 import get_string, get_float
+from math import pi
+
+def Count_field(name,x,y = None):
+    if name == 'circle':
+        return pi * x ** 2
+    elif name == 'triangle' or name == 'rhombus':
+        return x * y * 0.5
+    else:
+        return x * y
+
+name = get_string('Figure: ')
+name = name.lower()
+while True:
+    if name == 'circle' or name == 'rectangle' or name == 'rhombus' or name == 'triangle':
+        break
+    else:
+        name = get_string('Figure: ')
+        name = name.lower()
+
+x = get_float('First lenght: ')
+while x <= 0:
+    x = get_float('First lenght: ')
+y = 0
+if name != 'circle':
+    y = get_float('Second lenght: ')
+    while y <= 0:
+        y = get_float('Second lenght: ')
+
+print(f'Field = {Count_field(name,x,y):.2f}')

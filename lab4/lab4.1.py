4ac8a53d8515a9234b929c48d1c0ee54b7abcd77
michal1domanski <michal.domanski22@gmail.com>
2019-04-01 17:37:08 +0200

    implemented euler method as def

--- lab4/lab4.1.py
+++ lab4/lab4.1.py
@@ -1,10 +1,25 @@
 from cs50 import get_float
+from numpy import *
+from matplotlib.pyplot import *
 
-a = get_float('a: ')
-T = get_float('T: ')
-while T <= 0:
-    T = get_float('T; ')
-h = get_float('h: ')
-while h <= 0:
+def Euler():
+    a = get_float('a: ')
+    T = get_float('T: ')
+    while T <= 0:
+        T = get_float('T; ')
     h = get_float('h: ')
-initial_x = get_float('x: ')
+    while h <= 0:
+        h = get_float('h: ')
+    initial_x = get_float('x: ')
+
+    t = arange(0, T, h)
+    x = zeros(t.shape)
+    x[0] = initial_x
+
+    for i in range(t.size-1):
+        x[i+1] = x[i] + h * (1 * x[i])
+    
+    plot(t,x,'o')
+    show()
+
+Euler()

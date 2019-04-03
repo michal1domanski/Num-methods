a598eefad17c055b8d2901750c6515d26d6de827
michal1domanski <michal.domanski22@gmail.com>
2019-04-03 22:59:20 +0200

    Using classes for the first time

---
+++ lab3/lab3.1v3 - classes.py
@@ -0,0 +1,48 @@
+from cs50 import get_float, get_string
+from math import pi
+
+class Figure:
+    def __init__ (self,name):
+        self.name = name
+        self.side1 = []
+        
+
+    def side1(self,side1):
+        self.side1.append(side1)
+
+    def field(self,field):
+        self.field = field
+
+a = Figure('triangle')
+b = Figure('rhombus')
+c = Figure('circle')
+d = Figure('rectangle')
+e = Figure('trapeze')
+
+figure = get_string('What figure would you like to measure?: ')
+figure = figure.lower()
+if figure == 'circle':
+    c.side1.append(get_float('Perimeter: '))
+    c.field = pi*c.side1[0]**2
+    print(c.name,round(c.field,2),c.side1)
+elif figure == 'rhombus':
+    b.side1.append(get_float('Diagonal one: '))
+    b.side1.append(get_float('Diagonal two: '))
+    b.field = b.side1[0]*b.side1[1]/2
+    print(b.name,round(b.field,2),b.side1)
+elif figure == 'triangle':
+    a.side1.append(get_float('Base: '))
+    a.side1.append(get_float('Height: '))
+    a.field = a.side1[0]*a.side1[1]/2
+    print(a.name,round(a.field,2),a.side1)
+elif figure == 'rectangle':
+    d.side1.append(get_float('Side one: '))
+    d.side1.append(get_float('Side two: '))
+    d.field = d.side1[0]*b.side1[1]
+    print(d.name,round(d.field,2),d.side1)
+else:
+    e.side1.append(get_float('base one: '))
+    e.side1.append(get_float('base two: '))
+    e.side1.append(get_float('height: '))
+    e.field = ((e.side1[0]+e.side1[1])*e.side1[2])/2
+    print(e.name,round(e.field,2),e.side1)

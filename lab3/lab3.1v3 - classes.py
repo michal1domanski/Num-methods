8341c61ac46f003cf9ff2d813aeeb09db78f8cae
michal1domanski <michal.domanski22@gmail.com>
2019-04-07 23:58:30 +0200

    deleted comments

--- lab3/lab3.1v3 - classes.py
+++ lab3/lab3.1v3 - classes.py
@@ -30,42 +30,10 @@ class Figure():
             perimeter = 'Imposible, perhaps the archives are incomplete...'
         return perimeter
 
-a = Figure('trapeze',[12,13,2])
+a = Figure('trapeze',[12,13])
+
 print(round(a.field(),2))
 print(a.perimeter())
 
-# while i < 2:
-#     figure = get_string('What figure would you like to measure?: ')
-#     figure = figure.lower()
-#     if figure == 'circle':
-#         c.side1.append(get_float('Perimeter: '))
-#         c.field = pi*c.side1[0]**2
-#         print(c.name,round(c.field,2),c.side1)
-#         i+=1
-#     elif figure == 'rhombus':
-#         b.side1.append(get_float('Diagonal one: '))
-#         b.side1.append(get_float('Diagonal two: '))
-#         b.field = b.side1[0]*b.side1[1]/2
-#         print(b.name,round(b.field,2),b.side1)
-#         i+=1
-#     elif figure == 'triangle':
-#         a.side1.append(get_float('Base: '))
-#         a.side1.append(get_float('Height: '))
-#         a.field = a.side1[0]*a.side1[1]/2
-#         print(a.name,round(a.field,2),a.side1)
-#         i+=1
-#     elif figure == 'rectangle':
-#         d.side1.append(get_float('Side one: '))
-#         d.side1.append(get_float('Side two: '))
-#         d.field = d.side1[0]*b.side1[1]
-#         print(d.name,round(d.field,2),d.side1)
-#         i+=1
-#     else:
-#         e.side1.append(get_float('base one: '))
-#         e.side1.append(get_float('base two: '))
-#         e.side1.append(get_float('height: '))
-#         e.field = ((e.side1[0]+e.side1[1])*e.side1[2])/2
-#         print(e.name,round(e.field,2),e.side1)
-#         i+=1
 
 

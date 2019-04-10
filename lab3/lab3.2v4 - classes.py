64d0c6a6b3825bd7d87e6dcf83373162a82de205
michal1domanski <michal.domanski22@gmail.com>
2019-04-10 19:31:14 +0200

    sides change

--- lab3/lab3.2v4 - classes.py
+++ lab3/lab3.2v4 - classes.py
@@ -1,21 +1,20 @@
 from math import pi
 
 class Figure:
-    def __init__(self, figure, side1 = 0, side2 = 0, side3 = 0):
+    def __init__(self, figure, *sides):
         self.figure = figure
-        self.side1 = side1
-        self.side2 = side2
-        self.side3 = side3
+        self.sides = sides
 
     def c_field(self):
+        # dodaj tutaj fori i in range(2) zeby dwa razy zwrociło ci wartosć i dodaj compare fields do klasy
         if self.figure == 'circle':
-            field = pi * self.side1 ** 2
+            field = pi * self.sides[0] ** 2
         elif self.figure == 'rectangle':
-            field = self.side1 * self.side2
+            field = self.sides[0] * self.sides[1]
         elif self.figure == 'rhombus' or self.figure == 'triangle':
-            field = self.side1 * self.side2 * 0.5
+            field = self.sides[0] * self.sides[1] * 0.5
         elif self.figure == 'trapeze':
-            field = ((self.side1 + self.side2) * self.side3) * 0.5
+            field = ((self.sides[0] + self.sides[1]) * self.sides[2]) * 0.5
         return field
 
 def compare_fields(field1, field2):
@@ -27,18 +26,6 @@ def compare_fields(field1, field2):
         return print('fields are equal ',field1,' ',field2)
 
 figure1 = Figure('rhombus', 12,18)
-figure2 = Figure('trapeze', 10,12,10)
-if figure1.side1 < 0 or figure1.side2 < 0 or figure1.side3 < 0:
-    print('Your mom gay')
-    field1 = 0
-else:
-    field1 = figure1.c_field()
-
-if figure2.side1 < 0 or figure2.side2 < 0 or figure2.side3 < 0:
-    print('Your mom gay')
-    field2 = 0
-else:
-    field2 = figure2.c_field()
-
-compare_fields(field1,field2)
+figure2 = Figure('circle',12)
+print(Figure('rhombus',12,18,11).c_field())
     

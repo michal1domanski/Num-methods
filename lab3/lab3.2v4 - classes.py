328e157ca0d54e47936a9efeca0bd1904a6498ec
michal1domanski <michal.domanski22@gmail.com>
2019-04-11 10:05:36 +0200

    added checking upper and lower letters

--- lab3/lab3.2v4 - classes.py
+++ lab3/lab3.2v4 - classes.py
@@ -7,27 +7,26 @@ class Figure:
 
     def c_field(self):
         # dodaj tutaj fori i in range(2) zeby dwa razy zwrociło ci wartosć i dodaj compare fields do klasy
-        if self.figure == 'circle':
+        if self.figure.lower() == 'circle':
             field = pi * self.sides[0] ** 2
-        elif self.figure == 'rectangle':
+        elif self.figure.lower() == 'rectangle':
             field = self.sides[0] * self.sides[1]
-        elif self.figure == 'rhombus' or self.figure == 'triangle':
+        elif self.figure.lower() == 'rhombus' or self.figure == 'triangle':
             field = self.sides[0] * self.sides[1] / 2
-        elif self.figure == 'trapeze':
+        elif self.figure.lower() == 'trapeze':
             field = ((self.sides[0] + self.sides[1]) * self.sides[2]) / 2
         return round(field,2)
 
 def compare_fields(field1, field2):
     if field1 > field2:
-        return print('field 1 [', field1, '] is bigger [', figure1.figure, ']')
+        return print('field 1 [', field1, '] is bigger \n[', figure1.figure.upper(), ']')
     elif field1 < field2:
-        return print('field 2 [', field2, '] is bigger [',figure2.figure,']')
+        return print('field 2 [', field2, '] is bigger \n[',figure2.figure.upper(),']')
     else:
         return print('fields are equal ',field1,' ',field2)
 
-figure1 = Figure('trapeze', 13, 17)
+figure1 = Figure('CiRcLe', 13, 17, 12)
 figure2 = Figure('triangle', 13, 17)
 field1 = Figure.c_field(figure1)
 field2 = Figure.c_field(figure2)
 compare_fields(field1,field2)
-    

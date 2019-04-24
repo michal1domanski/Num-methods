9617330d8ea64fe9bef6de68ec8db40ee691f34b
michal1domanski <michal.domanski22@gmail.com>
2019-04-24 22:06:10 +0200

    better addjustment for 4.1

--- lab4/lab4.1.py
+++ lab4/lab4.1.py
@@ -4,8 +4,9 @@ from matplotlib.pyplot import *
 
 def Euler():
     a = 1
-    T = 5
+    T = 3
     h = 0.1
+    
     initial_x = 1
     t = arange(0, T, h)
     x = zeros(t.shape)
@@ -13,8 +14,19 @@ def Euler():
 
     for i in range(t.size-1):
         x[i+1] = x[i] + h * (1 * x[i])
-    
+
+    h = 0.001
+    initial_y = 1
+    r = arange(0,T,h)
+    y = zeros(r.shape)
+    y[0] = initial_y
+    for i in range(r.size-1):
+        y[i+1] = y[i] + h * (1 * y[i])
     plot(t,x,'-')
+    plot(r,y,'g-')
     show()
 
 Euler()
+
+
+

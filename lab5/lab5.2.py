bacb515b6377baf4286ecaabd8eae51bbb4865ee
michal1domanski <michal.domanski22@gmail.com>
2019-05-12 19:15:40 +0200

    creating random users with basic info

--- lab5/lab5.2.py
+++ lab5/lab5.2.py
@@ -1,10 +1,44 @@
 import requests
+import time
 
 # https://randomuser.me/api/?inc=id,name,login
 
+begin = time.time()
 
 response = requests.get("https://bitbay.net/API/Public/BTCPLN/ticker.json")
 data = response.json()
 best_bid=data['bid']
 best_ask=data['ask']
 print('bid:',best_bid,'ask:',best_ask)
+
+def osoba():
+    response = requests.get("https://randomuser.me/api/?inc=id,name,login")
+    data = response.json()
+    ide = data["results"][0]['id']['value']
+    username = data["results"][0]['login']['username']
+    first = data["results"][0]['name']['first']
+    last = data["results"][0]['name']['last']
+    return first, last, ide, username 
+
+i = 10
+osoby = []
+while len(osoby) < i:
+    try:
+        osoby.append(osoba())
+    except:
+        break
+
+for k in range(len(osoby)):
+    print(osoby[k])
+end = time.time()
+print(end - begin)
+
+
+# 2 Use https://randomuser.me API to download a random user data.
+# Create and store 100 random users with ids, username, name (first + last name) using this API (2p)
+# 3 Prepare a simulation of transactions between these users
+# Take random user and pair him/her with another one. Assume a random amount but take real world price. Sum up the transaction printing:
+# username1 exchanged X.XXX BTC with username2 for PLN YYYYY.YYY PLN. (2p)
+# Simulate real time - do not proceed all transactions at once. Try to make around 100 transactions per minute (2p)
+# Simulate user's assets. Creating a user assign random amount of a given currency. Take it into account while performing a transaction.
+# Remember to amend user's assets after the transaction. (2p)

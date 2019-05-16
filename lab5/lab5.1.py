390bd0c05238ca2dd9e7e55d50188f890dc422da
michal1domanski <michal.domanski22@gmail.com>
2019-05-16 19:38:20 +0200

    hot fixes during classes

--- lab5/lab5.1.py
+++ lab5/lab5.1.py
@@ -4,13 +4,13 @@ response = requests.get("https://bitbay.net/API/Public/BTCPLN/ticker.json")
 data = response.json()
 best_bid=data['bid']
 best_ask=data['ask']
-print('bid:',best_bid,'ask:',best_ask)
+print('bid bitbay:',best_bid,'ask bitbay:',best_ask)
 
 response2 = requests.get("https://www.bitmarket.pl/json/BTCPLN/ticker.json")
 data2 = response2.json()
 best_bid2 = data2['bid']
 best_ask2 = data2['ask']
-print('bid BTC:',best_bid2,'ask BTC:',best_ask2)
+print('bid bitmarket:',best_bid2,'ask bitmarket:',best_ask2)
 
 print('Selling:')
 
@@ -22,7 +22,7 @@ else:
 print('Buying:')
 
 if best_ask < best_ask2:
-    print('Bitbay is better for selling')
+    print('Bitbay is better for buying')
 else:
-    print('Bitmarket is better for selling')
+    print('Bitmarket is better for buying')
 
--- lab5/zadanko5.1.py
+++ lab5/zadanko5.1.py
@@ -1,56 +1,76 @@
 import requests
 from random import *
 import time
-k=0
-best_bid= requests.get('https://www.bitmarket.pl/json/BTCPLN/ticker.json')
-BTC=best_bid.json()
-kursBTC=float(BTC["ask"])
-kursPLN=1/kursBTC
 
+begin = time.time()
+
+k = 0
+z = 20
 wszyscy=[]
 
-for i in range(0,20):
-    uzytkownicy=[]
-    response = requests.get('https://randomuser.me/api/?inc=id,name,login')
-    uzytkownik = response.json()
+for i in range(0,z):
+    try:
+        
+        uzytkownicy=[]
+        response = requests.get('https://randomuser.me/api/?inc=id,name,login')
+        uzytkownik = response.json()
 
-    username = uzytkownik['results'][0]['login']['username']
-    first = uzytkownik['results'][0]['name']['first']
-    last = uzytkownik['results'][0]['name']['last']
-    uuid = uzytkownik['results'][0]['login']['uuid']
+        username = uzytkownik['results'][0]['login']['username']
+        first = uzytkownik['results'][0]['name']['first']
+        last = uzytkownik['results'][0]['name']['last']
+        uuid = uzytkownik['results'][0]['login']['uuid']
+        name = first + ' ' + last
 
-    uzytkownicy.append(first)
-    uzytkownicy.append(last)
-    uzytkownicy.append(username)
-    uzytkownicy.append(uuid)
+        uzytkownicy.append(first)
+        uzytkownicy.append(last)
+        uzytkownicy.append(username)
+        uzytkownicy.append(uuid)
     
-    randomBTC=round(kursBTC*random()*0.001,3)
-    uzytkownicy.append(randomBTC)
+        best_bid= requests.get('https://www.bitmarket.pl/json/BTCPLN/ticker.json')
+        BTC=best_bid.json()
+        kursBTC=float(BTC["ask"])
 
-    randomPLN=round(uniform(5000,150000),2)
-    uzytkownicy.append(randomPLN)
-    wszyscy.append(uzytkownicy)
+        randomBTC=round(kursBTC*random()*0.001,3)
+        uzytkownicy.append(randomBTC)
+
+        randomPLN=round(uniform(5000,150000),2)
+        uzytkownicy.append(randomPLN)
+        
+        uzytkownicy.append(name)
+
+        wszyscy.append(uzytkownicy)
+    except:
+        pass
 
 print(wszyscy)
-while k<20:
+
+while k<z:
+    
+    best_bid= requests.get('https://www.bitmarket.pl/json/BTCPLN/ticker.json')
+    BTC=best_bid.json()
+    kursBTC=float(BTC["ask"])
+    kursPLN=1/kursBTC
+    
     sprzedajacy=choice(wszyscy)
     x=sprzedajacy[4]*uniform(0.1,0.5)
     wymiana_btc_zl = x * kursBTC
-    print(sprzedajacy[2],'chce sprzedac ',round(x,3),' BTC')
+    print(sprzedajacy[2],'chce sprzedac',round(x,3),'BTC')
 
     while wymiana_btc_zl!=0:
+        
         kupujacy = choice(wszyscy)
 
-        while sprzedajacy[3] == kupujacy[3] or kupujacy[5]==0:
+        while sprzedajacy[3] == kupujacy[3]:
             kupujacy = choice(wszyscy)
 
         y = kupujacy[5]
+        
         if y>=wymiana_btc_zl:
             kupujacy[5]=kupujacy[5]-wymiana_btc_zl
             kupujacy[4]=kupujacy[4]+x
             sprzedajacy[5]=sprzedajacy[5]+wymiana_btc_zl
             sprzedajacy[4]=sprzedajacy[4]-x
-            print(sprzedajacy[2],"sprzedał ",round(wymiana_btc_zl*kursPLN,3)," BTC użytkownikowi ", kupujacy[2]," za ", round(wymiana_btc_zl,2), " zł")
+            print(sprzedajacy[2],"sprzedał",round(wymiana_btc_zl*kursPLN,3),"BTC użytkownikowi", kupujacy[2],"za", round(wymiana_btc_zl,2), "zł")
             wymiana_btc_zl=0
 
         else:
@@ -58,16 +78,17 @@ while k<20:
             kupujacy[4]=kupujacy[4]+y*kursPLN
             sprzedajacy[5]=sprzedajacy[5]+y
             sprzedajacy[4]=sprzedajacy[4]-y*kursPLN
-            print(sprzedajacy[2], "sprzedał ",round(y*kursPLN,3) , " BTC użytkownikowi ", kupujacy[2], " za ", round(y,2), " zł")
+            print(sprzedajacy[2], "sprzedał",round(y*kursPLN,3) , "BTC użytkownikowi", kupujacy[2], "za", round(y,2), "zł")
             wymiana_btc_zl = wymiana_btc_zl - y
             time.sleep(2 / 5)
-    print("Tranzakcja użytkownika", sprzedajacy[2]," skończona")
+    print("Tranzakcja użytkownika", sprzedajacy[2],"skończona")
     k=k+1
     time.sleep(2/5)
 
 print('-----------------------------------')
 print(wszyscy)
-
+end = time.time()
+print(end - begin)
 
 
 # Create and store 100 random users with ids, username, name (first + last name) using this API (2p)# fetch data from  API

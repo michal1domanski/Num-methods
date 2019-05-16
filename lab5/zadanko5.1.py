134dda3c387e26fd7e6054c293ab35ffa53af07e
michal1domanski <michal.domanski22@gmail.com>
2019-05-16 13:34:39 +0200

    better version

--- lab5/zadanko5.1.py
+++ lab5/zadanko5.1.py
@@ -1,73 +1,73 @@
 import requests
 from random import *
 import time
-
-# begin = time.time()
-
 k=0
-best_bid=22310
-wszyscy=[]
-kursBTC=22310
+best_bid= requests.get('https://www.bitmarket.pl/json/BTCPLN/ticker.json')
+BTC=best_bid.json()
+kursBTC=float(BTC["ask"])
 kursPLN=1/kursBTC
 
-while k < 100:
-    try:
-        uzytkownicy=[]
-        response = requests.get('https://randomuser.me/api/?inc=id,name,login')
-        uzytkownik = response.json()
+wszyscy=[]
 
-        username = uzytkownik['results'][0]['login']['username']
-        first = uzytkownik['results'][0]['name']['first']
-        last = uzytkownik['results'][0]['name']['last']
-        value = uzytkownik['results'][0]['id']['value']
+for i in range(0,20):
+    uzytkownicy=[]
+    response = requests.get('https://randomuser.me/api/?inc=id,name,login')
+    uzytkownik = response.json()
 
-        uzytkownicy.append(first)
-        uzytkownicy.append(last)
-        uzytkownicy.append(username)
-        uzytkownicy.append(value)
+    username = uzytkownik['results'][0]['login']['username']
+    first = uzytkownik['results'][0]['name']['first']
+    last = uzytkownik['results'][0]['name']['last']
+    uuid = uzytkownik['results'][0]['login']['uuid']
+
+    uzytkownicy.append(first)
+    uzytkownicy.append(last)
+    uzytkownicy.append(username)
+    uzytkownicy.append(uuid)
     
-        randomBTC=round(best_bid*random()*0.001,3)
-        uzytkownicy.append(randomBTC)
+    randomBTC=round(kursBTC*random()*0.001,3)
+    uzytkownicy.append(randomBTC)
 
-        randomPLN=round(uniform(5000,150000),2)
-        uzytkownicy.append(randomPLN)
-        wszyscy.append(uzytkownicy)
-        k += 1
-    except:
-        pass
+    randomPLN=round(uniform(5000,150000),2)
+    uzytkownicy.append(randomPLN)
+    wszyscy.append(uzytkownicy)
 
 print(wszyscy)
-begin = time.time()
-k = 0
-while k<100:
-    random1=choice(wszyscy)
-    random2=choice(wszyscy)
-    while random1==random2:
-        random1=choice(wszyscy)
-    x=random1[4]*uniform(0.1,0.5)
-    y=random2[5]
-    if x*kursBTC<y:
-        time.sleep(1/3)
-        wymiana=x*kursBTC
-        
-        random2[5]=y-wymiana
-        random2[4]=random2[4]+x
-        random1[5]=random1[5]+wymiana
-        random1[4]=random1[4]-x
+while k<20:
+    sprzedajacy=choice(wszyscy)
+    x=sprzedajacy[4]*uniform(0.1,0.5)
+    wymiana_btc_zl = x * kursBTC
+    print(sprzedajacy[2],'chce sprzedac ',round(x,3),' BTC')
 
-        print(random1[2], 'exchanged', x, 'BTC for', wymiana, 'PLN with', random2[2])
+    while wymiana_btc_zl!=0:
+        kupujacy = choice(wszyscy)
 
-        k+=1
+        while sprzedajacy[3] == kupujacy[3] or kupujacy[5]==0:
+            kupujacy = choice(wszyscy)
 
-    else:
-        pass
-    
+        y = kupujacy[5]
+        if y>=wymiana_btc_zl:
+            kupujacy[5]=kupujacy[5]-wymiana_btc_zl
+            kupujacy[4]=kupujacy[4]+x
+            sprzedajacy[5]=sprzedajacy[5]+wymiana_btc_zl
+            sprzedajacy[4]=sprzedajacy[4]-x
+            print(sprzedajacy[2],"sprzedał ",round(wymiana_btc_zl*kursPLN,3)," BTC użytkownikowi ", kupujacy[2]," za ", round(wymiana_btc_zl,2), " zł")
+            wymiana_btc_zl=0
+
+        else:
+            kupujacy[5]=0
+            kupujacy[4]=kupujacy[4]+y*kursPLN
+            sprzedajacy[5]=sprzedajacy[5]+y
+            sprzedajacy[4]=sprzedajacy[4]-y*kursPLN
+            print(sprzedajacy[2], "sprzedał ",round(y*kursPLN,3) , " BTC użytkownikowi ", kupujacy[2], " za ", round(y,2), " zł")
+            wymiana_btc_zl = wymiana_btc_zl - y
+            time.sleep(2 / 5)
+    print("Tranzakcja użytkownika", sprzedajacy[2]," skończona")
+    k=k+1
+    time.sleep(2/5)
+
 print('-----------------------------------')
-
 print(wszyscy)
 
-end = time.time()
-print(end - begin)
 
 
 # Create and store 100 random users with ids, username, name (first + last name) using this API (2p)# fetch data from  API
